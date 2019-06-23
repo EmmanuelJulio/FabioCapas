34a3d0ce1b4ae2002f45b7ed501ce27426124365
emmanueljulio <emajulio.ej@gmail.com>
2019-06-23 00:29:17 -0300

    Primer pull

---
+++ UiFabio/Loggin.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace UiFabio
+{
+    public partial class Loggin : Form
+    {
+        public Loggin()
+        {
+            InitializeComponent();
+        }
+        private void Txtpass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+            {
+                btnlogin.Focus();
+            }
+        }
+        private void Txtuser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+            {
+                txtpass.Focus();
+            }
+        }
+        private void Txtpass_Leave(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "Contraseña";
+                txtpass.ForeColor = Color.Silver;
+                txtpass.UseSystemPasswordChar = false;
+            }
+        }
+        private void Txtpass_Enter_1(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "Contraseña")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+        private void Txtuser_Enter_1(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "Usuario")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+            }
+        }
+        private void Txtuser_Leave_1(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "")
+            {
+                txtuser.Text = "Usuario";
+                txtuser.ForeColor = Color.Silver;
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void timerHora_Tick(object sender, EventArgs e)
+        {
+            HORA.Text = DateTime.Now.ToLongTimeString();
+            Dia.Text = DateTime.Now.ToLongDateString();
+            if (this.Opacity != 1)
+            {
+                this.Opacity +=  0.15;
+            }
+            
+           
+        }
+
+        private void Btnlogin_Click(object sender, EventArgs e)
+        {
+            MensajeError.Visible = false;
+            if (txtuser.Text == "Usuario")
+            {
+                MensajeError.Visible = true;
+                MensajeError.Text = "Debe ingresar un usuario";
+
+            }
+            else {
+                if (txtpass.Text == "Contraceña")
+                {
+                    MensajeError.Visible = true;
+                    MensajeError.Text = "Debe ingresar unacontraceña";
+                    
+                }
+            }
+            if(CapaNegocios.ClsUsuario.loggin(txtuser.Text, txtpass.Text))
+            {
+                MensajePers.message("bienvenido " + CapaNegocios.ClsUsuario.UsuarioActual.nombre,MensajePers.TipoMensaje.Acceso);
+            }
+            else
+            {
+                MensajePers.message("no se econtro el usuario", MensajePers.TipoMensaje.Error);
+            }
+            
+        }
+    }
+}
---
+++ UiFabio/Principal.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace UiFabio
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        public System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.Panelbot = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.Txt_sector = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.txt_usuario = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Botones = new System.Windows.Forms.Panel();
+            this.PanelForm = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.PnlSubMenus = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PnlModulos = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.Btn_volveropciones = new System.Windows.Forms.PictureBox();
+            this.Btn_volvermodulos = new System.Windows.Forms.PictureBox();
+            this.lbl_texto = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.LogoFabio = new System.Windows.Forms.PictureBox();
+            this.pnlTodo.SuspendLayout();
+            this.PanelTrabajo.SuspendLayout();
+            this.Panelbot.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Botones.SuspendLayout();
+            this.PanelForm.SuspendLayout();
+            this.PnlSubMenus.SuspendLayout();
+            this.PnlModulos.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_volveropciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_volvermodulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoFabio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelTrabajo
+            // 
+            this.PanelTrabajo.Controls.Add(this.Botones);
+            this.PanelTrabajo.Controls.Add(this.Panelbot);
+            // 
+            // Panelbot
+            // 
+            this.Panelbot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(112)))), ((int)(((byte)(183)))));
+            this.Panelbot.Controls.Add(this.pictureBox2);
+            this.Panelbot.Controls.Add(this.bunifuCustomLabel4);
+            this.Panelbot.Controls.Add(this.bunifuCustomLabel3);
+            this.Panelbot.Controls.Add(this.Txt_sector);
+            this.Panelbot.Controls.Add(this.txt_usuario);
+            this.Panelbot.Controls.Add(this.pictureBox1);
+            this.Panelbot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panelbot.Location = new System.Drawing.Point(0, 478);
+            this.Panelbot.Name = "Panelbot";
+            this.Panelbot.Size = new System.Drawing.Size(1067, 80);
+            this.Panelbot.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1033, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuCustomLabel4.AutoSize = true;
+            this.bunifuCustomLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel4.ForeColor = System.Drawing.Color.Silver;
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(989, 26);
+            this.bunifuCustomLabel4.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(64, 18);
+            this.bunifuCustomLabel4.TabIndex = 9;
+            this.bunifuCustomLabel4.Text = "21:38:48";
+            this.bunifuCustomLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel3.ForeColor = System.Drawing.Color.Silver;
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(862, 6);
+            this.bunifuCustomLabel3.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(193, 18);
+            this.bunifuCustomLabel3.TabIndex = 8;
+            this.bunifuCustomLabel3.Text = "Sabado 22 de junio del 2019";
+            this.bunifuCustomLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Txt_sector
+            // 
+            this.Txt_sector.AutoSize = true;
+            this.Txt_sector.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_sector.ForeColor = System.Drawing.Color.Silver;
+            this.Txt_sector.Location = new System.Drawing.Point(89, 26);
+            this.Txt_sector.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.Txt_sector.Name = "Txt_sector";
+            this.Txt_sector.Size = new System.Drawing.Size(110, 18);
+            this.Txt_sector.TabIndex = 7;
+            this.Txt_sector.Text = "Oficina Tecnica";
+            this.Txt_sector.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.AutoSize = true;
+            this.txt_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_usuario.ForeColor = System.Drawing.Color.Silver;
+            this.txt_usuario.Location = new System.Drawing.Point(89, 6);
+            this.txt_usuario.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(114, 18);
+            this.txt_usuario.TabIndex = 6;
+            this.txt_usuario.Text = "Emmanuel Julio";
+            this.txt_usuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Botones
+            // 
+            this.Botones.Controls.Add(this.PanelForm);
+            this.Botones.Controls.Add(this.PnlSubMenus);
+            this.Botones.Controls.Add(this.PnlModulos);
+            this.Botones.Controls.Add(this.pnlLogo);
+            this.Botones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Botones.Location = new System.Drawing.Point(0, 0);
+            this.Botones.Name = "Botones";
+            this.Botones.Size = new System.Drawing.Size(550, 478);
+            this.Botones.TabIndex = 1;
+            // 
+            // PanelForm
+            // 
+            this.PanelForm.Controls.Add(this.button3);
+            this.PanelForm.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelForm.Location = new System.Drawing.Point(272, 104);
+            this.PanelForm.Name = "PanelForm";
+            this.PanelForm.Size = new System.Drawing.Size(136, 374);
+            this.PanelForm.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // PnlSubMenus
+            // 
+            this.PnlSubMenus.Controls.Add(this.button2);
+            this.PnlSubMenus.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlSubMenus.Location = new System.Drawing.Point(136, 104);
+            this.PnlSubMenus.Name = "PnlSubMenus";
+            this.PnlSubMenus.Size = new System.Drawing.Size(136, 374);
+            this.PnlSubMenus.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // PnlModulos
+            // 
+            this.PnlModulos.Controls.Add(this.button1);
+            this.PnlModulos.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlModulos.Location = new System.Drawing.Point(0, 104);
+            this.PnlModulos.Name = "PnlModulos";
+            this.PnlModulos.Size = new System.Drawing.Size(136, 374);
+            this.PnlModulos.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.Btn_volveropciones);
+            this.pnlLogo.Controls.Add(this.Btn_volvermodulos);
+            this.pnlLogo.Controls.Add(this.lbl_texto);
+            this.pnlLogo.Controls.Add(this.LogoFabio);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(550, 104);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // Btn_volveropciones
+            // 
+            this.Btn_volveropciones.Image = ((System.Drawing.Image)(resources.GetObject("Btn_volveropciones.Image")));
+            this.Btn_volveropciones.Location = new System.Drawing.Point(35, 71);
+            this.Btn_volveropciones.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Btn_volveropciones.Name = "Btn_volveropciones";
+            this.Btn_volveropciones.Size = new System.Drawing.Size(24, 18);
+            this.Btn_volveropciones.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Btn_volveropciones.TabIndex = 7;
+            this.Btn_volveropciones.TabStop = false;
+            this.Btn_volveropciones.Visible = false;
+            // 
+            // Btn_volvermodulos
+            // 
+            this.Btn_volvermodulos.Image = ((System.Drawing.Image)(resources.GetObject("Btn_volvermodulos.Image")));
+            this.Btn_volvermodulos.Location = new System.Drawing.Point(5, 71);
+            this.Btn_volvermodulos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Btn_volvermodulos.Name = "Btn_volvermodulos";
+            this.Btn_volvermodulos.Size = new System.Drawing.Size(24, 18);
+            this.Btn_volvermodulos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Btn_volvermodulos.TabIndex = 6;
+            this.Btn_volvermodulos.TabStop = false;
+            this.Btn_volvermodulos.Visible = false;
+            // 
+            // lbl_texto
+            // 
+            this.lbl_texto.AutoSize = true;
+            this.lbl_texto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_texto.ForeColor = System.Drawing.Color.Silver;
+            this.lbl_texto.Location = new System.Drawing.Point(65, 71);
+            this.lbl_texto.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.lbl_texto.Name = "lbl_texto";
+            this.lbl_texto.Size = new System.Drawing.Size(66, 18);
+            this.lbl_texto.TabIndex = 5;
+            this.lbl_texto.Text = "Modulos";
+            this.lbl_texto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LogoFabio
+            // 
+            this.LogoFabio.Image = ((System.Drawing.Image)(resources.GetObject("LogoFabio.Image")));
+            this.LogoFabio.Location = new System.Drawing.Point(5, 6);
+            this.LogoFabio.Name = "LogoFabio";
+            this.LogoFabio.Size = new System.Drawing.Size(187, 61);
+            this.LogoFabio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LogoFabio.TabIndex = 0;
+            this.LogoFabio.TabStop = false;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.pnlTodo.ResumeLayout(false);
+            this.PanelTrabajo.ResumeLayout(false);
+            this.Panelbot.ResumeLayout(false);
+            this.Panelbot.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Botones.ResumeLayout(false);
+            this.PanelForm.ResumeLayout(false);
+            this.PnlSubMenus.ResumeLayout(false);
+            this.PnlModulos.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_volveropciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Btn_volvermodulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoFabio)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Botones;
+        private System.Windows.Forms.Panel PanelForm;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel PnlSubMenus;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel PnlModulos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.PictureBox Btn_volveropciones;
+        private System.Windows.Forms.PictureBox Btn_volvermodulos;
+        public Bunifu.Framework.UI.BunifuCustomLabel lbl_texto;
+        private System.Windows.Forms.PictureBox LogoFabio;
+        private System.Windows.Forms.Panel Panelbot;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        public Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        public Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        public Bunifu.Framework.UI.BunifuCustomLabel Txt_sector;
+        public Bunifu.Framework.UI.BunifuCustomLabel txt_usuario;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
